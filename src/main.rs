a03a7f16a8615b7aca120a818d267e27da037e36
readlnh <readlnh@163.com>
2019-10-15 18:37:37 +0800

    hash

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use rblockchainlib::*;
 
 
 fn main() {
-    let block = Block::new(0, now(), vec![0; 32], 0, "First block".to_owned());
+    let mut block = Block::new(0, now(), vec![0; 32], 0, "First block".to_owned());
     println!("{:?}", &block);
 
     let h = block.hash();
     println!("{:?}", &h);
+
+    block.hash = h;
+    println!("{:?}", &block);
 }
