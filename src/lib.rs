3cf1738136f82f394a75234d870c9da00ab6949a
readlnh <readlnh@163.com>
2019-10-21 17:32:52 +0800

    blockchain structure

--- src/lib.rs
+++ src/lib.rs
@@ -57,6 +57,29 @@ pub fn u128_bytes(u: &u128) -> [u8; 16] {
     ]
 }
 
+// convert last 16 bytes to u128
+pub fn difficulty_bytes_as_u128(v: &Vec<u8>) -> u128 {
+    ((v[31] as u128) << 8 * 0xf) |
+    ((v[30] as u128) << 8 * 0xe) |
+    ((v[29] as u128) << 8 * 0xd) |
+    ((v[28] as u128) << 8 * 0xc) |
+
+    ((v[27] as u128) << 8 * 0xb) |
+    ((v[26] as u128) << 8 * 0xa) |
+    ((v[25] as u128) << 8 * 0x9) |
+    ((v[24] as u128) << 8 * 0x8) |
+
+    ((v[23] as u128) << 8 * 0x7) |
+    ((v[22] as u128) << 8 * 0x6) |
+    ((v[21] as u128) << 8 * 0x5) |
+    ((v[20] as u128) << 8 * 0x4) |
+
+    ((v[19] as u128) << 8 * 0x3) |
+    ((v[18] as u128) << 8 * 0x2) |
+    ((v[17] as u128) << 8 * 0x1) |
+    ((v[16] as u128) << 8 * 0x0)
+}
+
 mod block;
 pub use crate::block::Block;
 mod hashable;
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ use rblockchainlib::*;
 
 
 fn main() {
-    let mut block = Block::new(0, now(), vec![0; 32], 0, "First block".to_owned());
+    let mut block = Block::new(0, now(), vec![0; 32], 0, "First block".to_owned(), 0x00000fffffffffffffffffffffffffff);
     println!("{:?}", &block);
 
-    let h = block.hash();
-    println!("{:?}", &h);
 
-    block.hash = h;
+    block.mine();
     println!("{:?}", &block);
 }
